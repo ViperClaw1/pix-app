2edc03b9abedd723e8462036a1d1c4ec6ce563d7
Nikita <48101012+ViperClaw1@users.noreply.github.com>
2026-03-09 10:58:31 +0400

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "kz.pixapp.pix_flutter"
+    namespace = "com.pix.pix"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "kz.pixapp.pix_flutter"
+        applicationId = "com.pix.pix"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
